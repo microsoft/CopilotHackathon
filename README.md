c3a327fa1883d019cc7eee9e7c6d951a62bd1e37
David Hernandez <david.hernandez@microsoft.com>
2023-04-13 15:50:03 +0200

    Added new functionality labs in nodejs scenario

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Proof of Concept for Copilot using Nodejs 
+# Activate GitHub Copilot using Nodejs 
 
-Demo project for running a Proof of Concept for Copilot viability
+Demo project for running labs to evaluate Copilot viability
 
 ## Pre-requisites
 
@@ -18,7 +18,10 @@ https://code.visualstudio.com/
 
 **Install mocha**
 
-run npm install --global mocha
+run:
+
+ npm install --global mocha
+ npm install axios
 
 **Install Docker**
 
@@ -26,7 +29,7 @@ https://docs.docker.com/engine/install/
 
 ## Goal
 
-The goal of a GitHub Copilot proof of concept is to learn how to use it.
+The goal of a GitHub Copilot Activate is to learn how to use it, using an exercise that consist of building a web server using Nodejs with different functionalities.
 
  Copilot is an AI-powered code assistant that helps developers write better code faster. It uses machine learning models trained on billions of lines of code to suggest whole lines or entire functions based on the context of what you’re working on. By using Copilot, you can learn how to write better code and improve your productivity.
 
@@ -35,46 +38,6 @@ Remember:
 - As you type GitHub Copilot will make suggestions, you can accept them by pressing Tab.
 - If nothing shows up after Copilot write some lines , press enter and wait a couple of seconds.
 - On Windows or Linux, press Ctrl + Enter, then click Open GitHub Copilot.
-
-## Exercise
-
-The exercise consist of building a web server using Nodejs that serves the request of various functionality.
-
-The requests that the server must attend are the following:
-
-**/get** : Return a hello world message
-
-
-**/daysBetweenDates**: Calculate days between two dates
-
-receive by query string 2 parameters date1 and date 2 , and calcualte the days that are between those two dates.
-
-**/validatephonenumber**: 
-Receive by querystring a parameter called phoneNumber 
-validate phoneNumber with Spanish format, for example +34666777888
-if phoneNumber is valid return "valid"
-if phoneNumber is not valid return "invalid"
-
-**validateSpanishDNI** 
-Receive by querystring a parameter called dni
-calculate DNI letter
-if DNI is valid return "valid"
-if DNI is not valid return "invalid"
-
-We will create automated tests to check that the functionality is correctly implemented.
-When the development is completed, we will build a container using Docker
-
-**returnColorCode**
-Receive by querystring a parameter called color
-
-read colors.json file and return the rgba field
-
-get color var from querystring
-
-iterate for each color in colors.json to find the color
-
-return the code.hex field
-
 
 ## Instructions
 
@@ -93,7 +56,7 @@ server is listening on port 3000
 
   1 passing (34ms)
 
-- In the NodeServer.js develop the rest of the methods described in the Exercise (do not forget to open color.json file in Visual Studio Code, so CoPilot get all the context to make better recommendations)
+- In the NodeServer.js develop the rest of the methods described in the Exercise described in the section below (do not forget to open color.json file in Visual Studio Code, so CoPilot get all the context to make better recommendations)
 - In the Test.js file add the methods to test the funcionality
 - Run the tests to verify that all is working 
 - Open the dockerfile file, and fill it, in order to create a docker container with a node image that can run the web server
@@ -105,3 +68,203 @@ So we can see how CoPilot based on the current file produces a curl command, to 
 - Also you can be more specific like: //run a curl command to test the daysBetweenDates method
 
 So it generates a test for a specific method 
+
+## Exercise
+
+The exercise consist of building a web server using Nodejs that serves the request of various functionality.
+
+The requests that the server must attend are the following:
+
+- **/Get** : 
+
+Return a hello world message
+
+
+- **/DaysBetweenDates**: 
+
+Calculate days between two dates
+
+receive by query string 2 parameters date1 and date 2 , and calcualte the days that are between those two dates.
+
+- **/Validatephonenumber**: 
+
+Receive by querystring a parameter called phoneNumber 
+validate phoneNumber with Spanish format, for example +34666777888
+if phoneNumber is valid return "valid"
+if phoneNumber is not valid return "invalid"
+
+- **/ValidateSpanishDNI**:
+
+Receive by querystring a parameter called dni
+calculate DNI letter
+if DNI is valid return "valid"
+if DNI is not valid return "invalid"
+
+We will create automated tests to check that the functionality is correctly implemented.
+When the development is completed, we will build a container using Docker
+
+- **/ReturnColorCode**:
+
+Receive by querystring a parameter called color
+
+read colors.json file and return the rgba field
+
+get color var from querystring
+
+iterate for each color in colors.json to find the color
+
+return the code.hex field
+
+- **/TellMeAJoke**:
+
+Make a call to the joke api and return a random joke using axios
+        
+
+- **/MoviesByDirector**:
+
+(this will require to browse to https://www.omdbapi.com/apikey.aspx and request a FREE API Key)
+
+Receive by querystring a parameter called director
+
+Make a call to the movie api  and return a list of movies of that director using axios
+
+Return the full list of movies
+
+- **/ParseUrl**:
+
+Retrieves a parameter from querystring called someurl
+
+Parse the url and return the protocol, host, port, path, querystring and hash
+
+Return the parsed host
+
+- **/ListFiles**:
+
+Get the current directory
+
+Get the list of files in the current directory
+
+Return the list of files
+
+- **/GetFullTextFile**:
+
+Read sample.txt and return lines that contains the word "Fusce"
+
+(becareful with this implementation, since this normally reads the full content of the file before analizing it, so memory usage is high and may fail when files are too big)
+
+- **/GetLineByLinefromtTextFile**:
+
+Read sample.txt line by line
+
+Create a promise to read the file line by line, and return a list of lines that contains the word "Fusce"
+
+Return the list of lines
+
+- **/CalculateMemoryConsumption**:
+
+Return the memory consumption of the process in GB, rounded to 2 decimals
+
+- **/MakeZipFile**:
+
+Using zlib create a zip file called sample.gz that contains sample.txt
+
+- **/RandomEuropeanCountry**:
+
+Make an array of european countries and its iso codes
+
+Return a random country from the array
+
+Return the country and its iso code
+
+
+## GitHub Copilot Labs exercises
+
+These tasks can be performed with the Copilot labs add-in, currently PREVIEW functionality, expect some bugs.
+
+
+Make sure to install the GitHub Copilot labs extension: https://marketplace.visualstudio.com/items?itemName=GitHub.copilot-labs
+
+Open GitHub Copilot extension to see all the available functionality.
+
+- **Explain**
+
+Select the line that has the regex in the validatePhoneNumber method, in EXPLAIN section click "Ask Copilot". You will see an explanation detailing what does each different notations in the regular expression.
+
+- **Language translation**
+
+Select some source code, like this line:
+
+    var randomCountry = countries[Math.floor(Math.random() * countries.length)];
+
+In "LANGUAGE TRANSLATION" section select python and click "As Copilot" button, you should see new code in python (sometimes you'll need to click twice in the button).
+
+- **Readable**
+
+Select the content of MakeZipFile
+
+In the BRUSHES section, click in "Readable", see how comments are added and also variables that have short names are renamed to a more understandable name.
+
+
+-- **Add Types**
+
+TBD
+
+-- **Fix Bug**
+
+In the exercise, there should be no bugs, since most of the code will be done by CoPilot. We can force some errors and then test the debug functionality.
+
+Force some errors like:
+
+In a for loop change the beginning to (change the 0 for a 1):
+
+    for (var i = 1
+
+select the text and in the "BRUSHES" section press the "Fix Bug" button.
+
+-- **Debug**
+
+Select some lines of text that contains variables, like:
+
+    var queryData = url.parse(req.url, true).query;
+    var color = queryData.color;
+    var colorFound = "not found";
+
+select the text and in the "BRUSHES" section press the "Debug" button.
+
+
+-- **Clean**
+
+TBD
+
+
+-- **List steps**
+
+Select some lines of code that do not have comments and in the  "BRUSHES" section press the "List steps" button.
+
+
+-- **Make robust**
+
+Select some text that comes from input, for example variables that come from querystring:
+
+        var queryData = url.parse(req.url, true).query;
+        var date1 = queryData.date1;
+        var date2 = queryData.date2;
+
+In the  "BRUSHES" section press the "Make robust" button, you will see that additional validation is added.
+
+-- **Chunk**
+
+TBD
+
+-- **Document**
+
+Select some line (e.g. a method or the beggining of the if clause)
+
+    else if (req.url.startsWith('/GetFullTextFile')) 
+
+In the  "BRUSHES" section press the "Document" button, you will see that comments explaining what the code does are added before the line.
+
+
+-- **Test Generation**
+
+TBD
--- completesolution/nodeserver.js
+++ completesolution/nodeserver.js
@@ -12,16 +12,7 @@ const server = http.createServer((req, res) => {
 
 
     
-     if (req.url.startsWith('/get')) {
-        const { query } = url.parse(req.url, true);
-        const { key } = query;
-
-        if (!key) {
-            res.end('key not passed');
-        } else {
-            res.end('hello ' + key);
-        }
-    } else if (req.url.startsWith('/daysBetweenDates')) {
+      if (req.url.startsWith('/DaysBetweenDates')) {
         //calculate days between two dates
 
         //get dates from querystring
@@ -39,7 +30,7 @@ const server = http.createServer((req, res) => {
         //convert to days and return
         res.end(Math.round(difference_ms / 86400000) + " days");
 
-    }     else if (req.url.startsWith('/validatephonenumber')) {
+    }     else if (req.url.startsWith('/Validatephonenumber')) {
 
         //get phoneNumber var from querystring
         var queryData = url.parse(req.url, true).query;
@@ -57,7 +48,7 @@ const server = http.createServer((req, res) => {
         else {
             res.end("invalid");
         }
-    } else if (req.url.startsWith('/validateSpanishDNI')) {
+    } else if (req.url.startsWith('/ValidateSpanishDNI')) {
         var queryData = url.parse(req.url, true).query;
         var dni = queryData.dni;
 
@@ -74,7 +65,8 @@ const server = http.createServer((req, res) => {
         else {
             res.end("invalid");
         }
-    } else if (req.url.startsWith('/returnColorCode')) {
+        
+    } else if (req.url.startsWith('/ReturnColorCode')) {
 
         //read colors.json file and return the rgba field
         var colors = fs.readFileSync('colors.json', 'utf-8');
@@ -95,9 +87,244 @@ const server = http.createServer((req, res) => {
 
         res.end(colorFound);
 
-    } else {
-        res.end('not found');
     }
+    else if (req.url.startsWith('/SendEmail')) {
+    }
+    else if (req.url.startsWith('/TellMeAJoke')) {
+
+        //make a call to the joke api and return a random joke using axios
+        const axios = require('axios');
+
+        axios.get('https://official-joke-api.appspot.com/random_joke')
+            .then(function (response) {
+                // handle success
+                res.end(response.data.setup + " " + response.data.punchline);
+            }
+            )
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            })
+            .then(function () {
+                // always executed
+            });
+    }
+
+    //method that gets the name of a director and retrieves from an api the list of movies of that director
+    else if (req.url.startsWith('/MoviesByDirector')) {
+
+        //get a director name from querystring
+        var queryData = url.parse(req.url, true).query;
+        var director = queryData.director;
+
+         //make a call to the movie api omdbapi.com and return a list of movies of that director using axios
+        const axios = require('axios');
+
+        axios.get('http://www.omdbapi.com/?apikey=XXXXXXX&s=' + director)
+            .then(function (response) {
+                
+                //return the full list of movies
+                var movies = "";
+                for (var i = 0; i < response.data.Search.length; i++) {
+                    movies = movies + response.data.Search[i].Title + ", ";
+                }
+
+                res.end(movies);
+                
+            }
+            )
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            }
+            )
+            .then(function () {
+                // always executed
+            }
+            );
+
+
+  
+    }
+    //If url equals to ParseUrl
+    else if (req.url.startsWith('/ParseUrl')) {
+
+        //retrieves a parameter from querystring called someurl
+        var queryData = url.parse(req.url, true).query;
+        var someUrl = queryData.someurl;
+
+        //parse the url and return the protocol, host, port, path, querystring and hash
+        var urlObj = new URL(someUrl);
+
+        var protocol = urlObj.protocol;
+        var host = urlObj.host;
+        var port = urlObj.port;
+        var path = urlObj.pathname;
+        var querystring = urlObj.search;
+        var hash = urlObj.hash;
+
+        //return the parsed host
+        res.end("host: " + host );
+
+    }
+//if url contains listFiles in current directory
+    else if (req.url.startsWith('/ListFiles')) {
+
+        //get the current directory
+        var currentDir = __dirname;
+
+        //get the list of files in the current directory
+        var files = fs.readdirSync(currentDir);
+
+        //return the list of files
+        res.end(files.toString());
+
+    }
+    else if (req.url.startsWith('/GetFullTextFile')) {
+
+        //read sample.txt and return lines that contains the word "Fusce"
+        var text = fs.readFileSync('sample.txt', 'utf-8');
+        var lines = text.split("\r");
+
+        var linesFound = "";
+        for (var i = 1; i < lines.length; i++) {
+            if (lines[i].includes("Fusce")) {
+                linesFound = linesFound + lines[i] + ", ";
+            }
+        }
+
+        res.end(linesFound);
+
+
+    }
+    else if (req.url.startsWith('/GetLineByLinefromtTextFile')) {
+    
+        //read sample.txt line by line
+        var lineReader = require('readline').createInterface({
+            input: require('fs').createReadStream('sample.txt')
+        });
+
+        //create a promise to read the file line by line, and return a list of lines that contains the word "Fusce"
+        var promise = new Promise(function (resolve, reject) {
+            var lines = [];
+            lineReader.on('line', function (line) {
+                if (line.includes("Fusce")) {
+                    lines.push(line);
+                }
+            });
+            lineReader.on('close', function () {
+                resolve(lines);
+            });
+        });
+
+        //return the list of lines
+        promise.then(function (lines) {
+            res.end(lines.toString());
+        });
+    }
+    else if (req.url.startsWith('/CalculateMemoryConsumption')) {
+    
+            //return the memory consumption of the process in GB, rounded to 2 decimals
+            var memory = process.memoryUsage().heapUsed / 1024 / 1024;
+
+            res.end(memory.toFixed(2) + " GB");
+
+    }
+    else if (req.url.startsWith('/MakeZipFile')) {
+
+        //using zlib create a zip file called sample.gz that contains sample.txt
+        var zlib = require('zlib');
+
+        var gzip = zlib.createGzip();
+        var input = fs.createReadStream('sample.txt');
+        var output = fs.createWriteStream('sample.gz');
+
+        input.pipe(gzip).pipe(output);
+
+        res.end("sample.gz created");
+
+    }
+    else if (req.url.startsWith('/RandomEuropeanCountry')) {
+
+        //make an array of european countries and its iso codes
+        var countries = [
+            { country: "Italy", iso: "IT" },
+            { country: "France", iso: "FR" },
+            { country: "Spain", iso: "ES" },
+            { country: "Germany", iso: "DE" },
+            { country: "United Kingdom", iso: "GB" },
+            { country: "Greece", iso: "GR" },
+            { country: "Portugal", iso: "PT" },
+            { country: "Romania", iso: "RO" },
+            { country: "Bulgaria", iso: "BG" },
+            { country: "Croatia", iso: "HR" },
+            { country: "Czech Republic", iso: "CZ" },
+            { country: "Denmark", iso: "DK" },
+            { country: "Estonia", iso: "EE" },
+            { country: "Finland", iso: "FI" },
+            { country: "Hungary", iso: "HU" },
+            { country: "Ireland", iso: "IE" },
+            { country: "Latvia", iso: "LV" },
+            { country: "Lithuania", iso: "LT" },
+            { country: "Luxembourg", iso: "LU" },
+            { country: "Malta", iso: "MT" },
+            { country: "Netherlands", iso: "NL" },
+            { country: "Poland", iso: "PL" },
+            { country: "Slovakia", iso: "SK" },
+            { country: "Slovenia", iso: "SI" },
+            { country: "Sweden", iso: "SE" },
+            { country: "Belgium", iso: "BE" },
+            { country: "Austria", iso: "AT" },
+            { country: "Switzerland", iso: "CH" },
+            { country: "Cyprus", iso: "CY" },
+            { country: "Iceland", iso: "IS" },
+            { country: "Norway", iso: "NO" },
+            { country: "Albania", iso: "AL" },
+            { country: "Andorra", iso: "AD" },
+            { country: "Armenia", iso: "AM" },
+            { country: "Azerbaijan", iso: "AZ" },
+            { country: "Belarus", iso: "BY" },
+            { country: "Bosnia and Herzegovina", iso: "BA" },
+            { country: "Georgia", iso: "GE" },
+            { country: "Kazakhstan", iso: "KZ" },
+            { country: "Kosovo", iso: "XK" },
+            { country: "Liechtenstein", iso: "LI" },
+            { country: "Macedonia", iso: "MK" },
+            { country: "Moldova", iso: "MD" },
+            { country: "Monaco", iso: "MC" },
+            { country: "Montenegro", iso: "ME" },
+            { country: "Russia", iso: "RU" },
+            { country: "San Marino", iso: "SM" },
+            { country: "Serbia", iso: "RS" },
+            { country: "Turkey", iso: "TR" },
+            { country: "Ukraine", iso: "UA" },
+            { country: "Vatican City", iso: "VA" }
+        ];
+
+        //return a random country from the array
+        var randomCountry = countries[Math.floor(Math.random() * countries.length)];
+
+        //return the country and its iso code
+        res.end(randomCountry.country + " " + randomCountry.iso);
+
+    }
+    else if (req.url.startsWith('/Get')) {
+        const { query } = url.parse(req.url, true);
+        const { key } = query;
+
+        if (!key) {
+            res.end('key not passed');
+        } else {
+            res.end('hello ' + key);
+        }
+    } 
+    else {
+        res.end('Called method not found');
+    }
+
+
+
+
 
 });
 
@@ -106,6 +333,11 @@ server.listen(3000, () => {
 });
 
 
+
+
 //write command line to generate package.json
 //npm init -y
+
+//write curl command to getMoviesByDirector
+//curl http://localhost:3000/getMoviesByDirector?director=Quentin%20Tarantino
 
