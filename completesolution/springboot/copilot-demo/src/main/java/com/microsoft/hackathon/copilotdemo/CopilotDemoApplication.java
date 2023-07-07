d41450e9b28943152a3b1fc867f5ad1e64b4779f
dsanchor <sancheski@gmail.com>
2023-07-07 06:38:49 +0200

    Added Spring Boot basic scenario

---
+++ completesolution/springboot/copilot-demo/src/main/java/com/microsoft/hackathon/copilotdemo/CopilotDemoApplication.java
@@ -0,0 +1,13 @@
+package com.microsoft.hackathon.copilotdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopilotDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CopilotDemoApplication.class, args);
+	}
+
+}
